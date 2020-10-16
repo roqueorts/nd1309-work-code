f448e322635c75783d6cba6e2068b2494682af06
Roque <roqueorts@gmail.com>
2020-10-16 15:51:00 +0200

    Sample token and some node modules updates

--- Course_Identity_And_Smart_Contracts/SampleToken/truffle-config.js
+++ Course_Identity_And_Smart_Contracts/SampleToken/truffle-config.js
@@ -19,30 +19,29 @@
  */
 
 const HDWalletProvider = require('truffle-hdwallet-provider');
-const infuraKey = "<Infura ProjectID>";
+const infuraKey = 'to change';
 //
 // const fs = require('fs');
-const mnemonic = "<METAMASK SEED>";
+var mnemonic = 'to change';
 
 module.exports = {
-
   networks: {
     development: {
-      host: "127.0.0.1",     // Localhost (default: none)
-      port: 9545,            // Standard Ethereum port (default: none)
-      network_id: "*",       // Any network (default: none)
-     },
+      host: '127.0.0.1', // Localhost (default: none)
+      port: 9545, // Standard Ethereum port (default: none)
+      network_id: '*' // Any network (default: none)
+    },
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
     rinkeby: {
       provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
-        network_id: 4,       // rinkeby's id
-        gas: 4500000,        // rinkeby has a lower block limit than mainnet
-        gasPrice: 10000000000
+      network_id: 4, // rinkeby's id
+      gas: 4500000, // rinkeby has a lower block limit than mainnet
+      gasPrice: 10000000000
       // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
       // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    },
+    }
   },
 
   // Set default mocha options here, use special reporters etc.
@@ -64,4 +63,4 @@ module.exports = {
       // }
     }
   }
-}
+};
